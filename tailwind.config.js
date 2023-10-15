84d61bb631a01f4f4a93a99625505c0f62b7e620
Irek <ir.jn.kr@gmmail.com>
2023-10-15 22:33:34 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Rubik', 'sans-serif'],
+			},
+			colors: {
+				darkSlateGrey: 'hsl(234, 29%, 20%)',
+				blue: 'hsl(246, 80%, 60%)',
+				lightOrange: 'hsl(15, 100%, 70%)',
+				softBlue: 'hsl(195, 74%, 62%)',
+				lightRed: 'hsl(348, 100%, 68%)',
+				limeGreen: 'hsl(145, 58%, 55%)',
+				violet: 'hsl(264, 64%, 52%)',
+				softOrange: 'hsl(43, 84%, 65%)',
+				veryDarkBlue: 'hsl(226, 43%, 10%)',
+				darkBlue: 'hsl(235, 46%, 20%)',
+				desaturatedBlue: 'hsl(235, 45%, 61%)',
+				paleBlue: 'hsl(236, 100%, 87%)',
+			},
+		},
+	},
+	plugins: [],
+};
+// module.exports = {
+// 	content: ['./src/**/*.{html,js}'],
+// 	theme: {
+// 		extend: {
+
+// 			colors: {
+// 				darkSlateGrey: 'hsl(234, 29%, 20%)',
+// 				blue: 'hsl(246, 80%, 60%)',
+// 				lightOrange: 'hsl(15, 100%, 70%)',
+// 				softBlue: 'hsl(195, 74%, 62%)',
+// 				lightRed: 'hsl(348, 100%, 68%)',
+// 				limeGreen: 'hsl(145, 58%, 55%)',
+// 				violet: 'hsl(264, 64%, 52%)',
+// 				softOrange: 'hsl(43, 84%, 65%)',
+// 				veryDarkBlue: 'hsl(226, 43%, 10%)',
+// 				darkBlue: 'hsl(235, 46%, 20%)',
+// 				desaturatedBlue: 'hsl(235, 45%, 61%)',
+// 				paleBlue: 'hsl(236, 100%, 87%)',
+// 			},
+// 		},
+// 	},
+// 	plugins: [],
+// };
